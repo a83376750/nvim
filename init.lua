7795d88a2871996f73590ec80ec8188057a2cb58
sai <394315001@qq.com>
2024-11-14 15:58:16 +0800

    添加tab缩进

--- init.lua
+++ init.lua
@@ -7,3 +7,9 @@ require'lspconfig'.clangd.setup{
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto","rs" }
 }
 require'fany.include'
+
+vim.opt.expandtab = false   -- 使用实际的制表符（tab）
+vim.opt.tabstop = 4         -- 设置 tab 键宽度为 4
+vim.opt.shiftwidth = 4      -- 设置自动缩进的宽度为 4
+vim.opt.softtabstop = 4     -- 设置编辑时表现为 4 个字符宽度
+vim.opt.guifont = "FiraCode:h14"
